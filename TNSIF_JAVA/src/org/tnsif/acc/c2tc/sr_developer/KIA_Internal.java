b9163899c51053839a69c147e17014184c4c8a21
ASUS <ASUS@LAPTOP-3RPPLF0O>
2026-07-20 12:43:47 +0530

    first commit

---
+++ TNSIF_JAVA/src/org/tnsif/acc/c2tc/sr_developer/KIA_Internal.java
@@ -0,0 +1,12 @@
+package org.tnsif.acc.c2tc.sr_developer;
+import jr_devoloper.*;
+public class KIA_Internal {
+
+	public static void main(String[] args) {
+		KIA_Airbags airbag=new KIA_Airbags();
+		airbag.disp_airbags();
+		
+
+	}
+
+}
