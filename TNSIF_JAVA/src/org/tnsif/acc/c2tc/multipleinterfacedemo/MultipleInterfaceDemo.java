69748fe75d8595e519522c62af99ac869812211d
ASUS <ASUS@LAPTOP-3RPPLF0O>
2026-08-12 12:15:21 +0530

    first commit

---
+++ TNSIF_JAVA/src/org/tnsif/acc/c2tc/multipleinterfacedemo/MultipleInterfaceDemo.java
@@ -0,0 +1,29 @@
+package org.tnsif.acc.c2tc.multipleinterfacedemo;
+
+class SmartPhone implements Camera, MusicPlayer
+{
+
+	@Override
+	public void playmusic() {
+		System.out.println("Playing a music in a smartphone");
+		
+	}
+
+	@Override
+	public void takephoto() {
+		System.out.println("Taking photo using smartphone");
+		
+	}
+	
+}
+
+public class MultipleInterfaceDemo 
+{
+
+	public static void main(String[] args) {
+		SmartPhone smartphone=new SmartPhone();
+		smartphone.playmusic();
+		smartphone.takephoto();
+	}
+
+}
